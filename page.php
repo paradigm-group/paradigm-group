f3e241e29d17e1ebab73ac392bc3001547ff642e
Jack Machin <jack.machin125@gmail.com>
2016-10-21 09:04:21 +0100

    Big commitment issues

---
+++ page.php
@@ -0,0 +1,53 @@
+<?php get_header(); ?>
+
+    <div class="content">
+
+        <div id="inner-content" class="container">
+
+            <div class="main" role="main">
+
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+                    <header class="article-header">
+
+                        <h1 class="entry-title page-title" itemprop="headline">
+                            <?php the_title(); ?>
+                        </h1>
+                        <?php if (is_front_page()) {} else {
+                            if ( function_exists('yoast_breadcrumb') ) {
+                                yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+                            }
+                        } ?>
+
+                    </header> <?php // end article header ?>
+
+                    <div class="entry-content" itemprop="articleBody">
+                        <?php
+                            // the content (pretty self explanatory huh)
+                            the_content();
+                        ?>
+                    </div> <?php // end article section ?>
+
+                    <footer class="article-footer">
+
+                    </footer> <?php // end article footer ?>
+
+                </article>
+
+            <?php endwhile; else : ?>
+
+                <?php get_template_part ('partials/no-post-found');?>
+
+            <?php endif; ?>
+
+            </div>
+
+            <?php  get_sidebar(); ?>
+
+        </div>
+
+    </div>
+
+<?php get_footer(); ?>
