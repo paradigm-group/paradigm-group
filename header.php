a44d4f328188c14ed3020b5bf7daee52c5b05368
Jack Machin <jack.machin125@gmail.com>
2016-10-26 10:27:15 +0100

    Try this menu

--- header.php
+++ header.php
@@ -52,40 +52,5 @@
                 <?php get_sidebar ('header');?>
 
             </div>
-            <nav role="navigation" class="navigation">
-
-                <?php if (is_page_template('page-members.php')) {
-
-                    wp_nav_menu(array(
-                        'container' => 'div',                               // remove nav container
-                        'container_class' => 'container',                   // class of container (should you choose to use it)
-                        'menu' => __( 'Members Menu', 'paradigm-group' ),   // nav name
-                        'menu_class' => 'nav top-nav',                      // adding custom nav class
-                        'theme_location' => 'members-nav',                  // where it's located in the theme
-                        'before' => '',                                     // before the menu
-                        'after' => '',                                      // after the menu
-                        'link_before' => '',                                // before each link
-                        'link_after' => '',                                 // after each link
-                        'depth' => 0,                                       // limit the depth of the nav
-                        'fallback_cb' => ''                                 // fallback function (if there is one)
-                    ));
-
-                } else {
-
-                    wp_nav_menu(array(
-                        'container' => 'div',                               // remove nav container
-                        'container_class' => 'container',                   // class of container (should you choose to use it)
-                        'menu' => __( 'Main Menu', 'paradigm-group' ),      // nav name
-                        'menu_class' => 'nav top-nav',                      // adding custom nav class
-                        'theme_location' => 'main-nav',                     // where it's located in the theme
-                        'before' => '',                                     // before the menu
-                        'after' => '',                                      // after the menu
-                        'link_before' => '',                                // before each link
-                        'link_after' => '',                                 // after each link
-                        'depth' => 0,                                       // limit the depth of the nav
-                        'fallback_cb' => ''                                 // fallback function (if there is one)
-                    ));
-                } ?>
-
-            </nav>
+            <?php get_template_part ('partials/navigation');?>
         </header>
--- partials/navigation.php
+++ partials/navigation.php
@@ -1,17 +1,42 @@
-<nav role="navigation">
+<?php
+/*
+*
+* Navigation fun - Different page templates, different menus
+*
+*/?>
+<nav role="navigation" class="navigation">
 
-  <?php wp_nav_menu(array(
-    'container' => 'div',                           // remove nav container
-    'container_class' => 'wrapper',                 // class of container (should you choose to use it)
-    'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-    'menu_class' => 'nav top-nav cf',               // adding custom nav class
-    'theme_location' => 'main-nav',                 // where it's located in the theme
-    'before' => '',                                 // before the menu
-    'after' => '',                                  // after the menu
-    'link_before' => '',                            // before each link
-    'link_after' => '',                             // after each link
-    'depth' => 0,                                   // limit the depth of the nav
-    'fallback_cb' => ''                             // fallback function (if there is one)
-  )); ?>
+    <?php if (is_page_template('page-members.php')) {
+
+        wp_nav_menu(array(
+            'container' => 'div',                               // remove nav container
+            'container_class' => 'container',                   // class of container (should you choose to use it)
+            'menu' => __( 'Members Menu', 'paradigm-group' ),   // nav name
+            'menu_class' => 'nav top-nav',                      // adding custom nav class
+            'theme_location' => 'members-nav',                  // where it's located in the theme
+            'before' => '',                                     // before the menu
+            'after' => '',                                      // after the menu
+            'link_before' => '',                                // before each link
+            'link_after' => '',                                 // after each link
+            'depth' => 0,                                       // limit the depth of the nav
+            'fallback_cb' => ''                                 // fallback function (if there is one)
+        ));
+
+    } else {
+
+        wp_nav_menu(array(
+            'container' => 'div',                               // remove nav container
+            'container_class' => 'container',                   // class of container (should you choose to use it)
+            'menu' => __( 'Main Menu', 'paradigm-group' ),      // nav name
+            'menu_class' => 'nav top-nav',                      // adding custom nav class
+            'theme_location' => 'main-nav',                     // where it's located in the theme
+            'before' => '',                                     // before the menu
+            'after' => '',                                      // after the menu
+            'link_before' => '',                                // before each link
+            'link_after' => '',                                 // after each link
+            'depth' => 0,                                       // limit the depth of the nav
+            'fallback_cb' => ''                                 // fallback function (if there is one)
+        ));
+    } ?>
 
 </nav>
