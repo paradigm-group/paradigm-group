d7c06419d07737ee9bf5e03a9855af3a2c9dde53
Jack Machin <jack.machin125@gmail.com>
2016-10-26 10:13:57 +0100

    Header

--- header.php
+++ header.php
@@ -54,19 +54,21 @@
             </div>
             <nav role="navigation" class="navigation">
 
-            <?php wp_nav_menu(array(
-                'container' => 'div',                           // remove nav container
-                'container_class' => 'container',                 // class of container (should you choose to use it)
-                'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-                'menu_class' => 'nav top-nav',               // adding custom nav class
-                'theme_location' => 'main-nav',                 // where it's located in the theme
-                'before' => '',                                 // before the menu
-                'after' => '',                                  // after the menu
-                'link_before' => '',                            // before each link
-                'link_after' => '',                             // after each link
-                'depth' => 0,                                   // limit the depth of the nav
-                'fallback_cb' => ''                             // fallback function (if there is one)
-            )); ?>
+                <?php if (is_page_template('members')) { echo '// this is the members template'; } else {} ?>
+
+                <?php wp_nav_menu(array(
+                    'container' => 'div',                           // remove nav container
+                    'container_class' => 'container',                 // class of container (should you choose to use it)
+                    'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+                    'menu_class' => 'nav top-nav',               // adding custom nav class
+                    'theme_location' => 'main-nav',                 // where it's located in the theme
+                    'before' => '',                                 // before the menu
+                    'after' => '',                                  // after the menu
+                    'link_before' => '',                            // before each link
+                    'link_after' => '',                             // after each link
+                    'depth' => 0,                                   // limit the depth of the nav
+                    'fallback_cb' => ''                             // fallback function (if there is one)
+                )); ?>
 
             </nav>
         </header>
