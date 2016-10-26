39fbbea144fbe2e39c0f8985edd87b90cdcd7833
Jack Machin <jack.machin125@gmail.com>
2016-10-26 11:21:12 +0100

    Members area

--- page-members.php
+++ page-members.php
@@ -11,51 +11,51 @@
 <?php get_header(); ?>
     <div class="content">
 
-        <div id="inner-content" class="container">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-            <div class="main" role="main">
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <header class="article-header page-header">
 
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+                <div class="container">
 
-                    <header class="article-header">
+                    <h1 class="entry-title page-title" itemprop="headline">
+                        <?php the_title(); ?>
+                    </h1>
 
-                        <h1 class="entry-title page-title" itemprop="headline">
-                            <?php the_title(); ?>
-                        </h1>
-                        <?php if (is_front_page()) {} else {
-                            if ( function_exists('yoast_breadcrumb') ) {
-                                yoast_breadcrumb('<p id="breadcrumbs">','</p>');
-                            }
-                        } ?>
+                </div>
 
-                    </header> <?php // end article header ?>
+            </header> <?php // end article header ?>
 
-                    <div class="entry-content" itemprop="articleBody">
-                        <?php
-                            // the content (pretty self explanatory huh)
-                            the_content();
-                        ?>
-                    </div> <?php // end article section ?>
+            <div class="container">
 
-                    <footer class="article-footer">
+                <div class="main entry-content" itemprop="articleBody">
 
-                    </footer> <?php // end article footer ?>
+                            <?php if (is_front_page()) {} else {
+                    if ( function_exists('yoast_breadcrumb') ) {
+                        yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+                    }
+                } ?>
+                    <?php
+                        // the content (pretty self explanatory huh)
+                        the_content();
+                    ?>
+                </div> <?php // end article section ?>
 
-                </article>
+                <footer class="article-footer">
 
-            <?php endwhile; else : ?>
+                </footer> <?php // end article footer ?>
 
-                <?php get_template_part ('partials/no-post-found');?>
-
-            <?php endif; ?>
+                <?php get_sidebar('members'); ?>
 
             </div>
+        </article>
 
-            <?php get_sidebar('members'); ?>
+    <?php endwhile; else : ?>
 
-        </div>
+        <?php get_template_part ('partials/no-post-found');?>
+
+    <?php endif; ?>
 
     </div>
 
