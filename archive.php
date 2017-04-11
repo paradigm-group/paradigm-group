ad576debd654097867deba84bf8f739eeb4649fd
Jack Machin <jack.machin125@gmail.com>
2017-04-11 15:54:42 +0100

    Archive

--- archive.php
+++ archive.php
@@ -74,12 +74,6 @@
 
                     <footer class="article-footer">
 
-                      <?php get_template_part ('partials/comment-count'); ?>
-
-                      <?php printf( '<p class="footer-category">' . __('Filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
-
-                      <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
                     </footer>
 
                 </article>
