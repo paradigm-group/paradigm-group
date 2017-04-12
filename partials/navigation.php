f09556fdb167bbebd4e3b413c2c6428bbd0afac3
Jack Machin <jack.machin125@gmail.com>
2017-04-12 10:43:36 +0100

    manual

--- partials/navigation.php
+++ partials/navigation.php
@@ -38,6 +38,22 @@
             'fallback_cb' => ''                                 // fallback function (if there is one)
         ));
 
+    } elseif (is_page_template('page-manual.php')) {
+
+        wp_nav_menu(array(
+            'container' => 'div',                               // remove nav container
+            'container_class' => 'container',                   // class of container (should you choose to use it)
+            'menu' => __( 'Members Menu', 'paradigm-group' ),   // nav name
+            'menu_class' => 'nav top-nav',                      // adding custom nav class
+            'theme_location' => 'members-nav',                  // where it's located in the theme
+            'before' => '',                                     // before the menu
+            'after' => '',                                      // after the menu
+            'link_before' => '',                                // before each link
+            'link_after' => '',                                 // after each link
+            'depth' => 0,                                       // limit the depth of the nav
+            'fallback_cb' => ''                                 // fallback function (if there is one)
+        ));
+
     } elseif (is_tax ('target_cat')) {
 
         wp_nav_menu(array(
